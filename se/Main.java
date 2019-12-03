8adc3754a83c9ae7b8388971ba20733a4f7d469f
hijerry17 <hijerry17@naver.com>
2019-12-03 16:07:18 +0900

    Update Main.java

--- se/Main.java
+++ se/Main.java
@@ -3,6 +3,7 @@ package se;
 // 1: remote by hijerry17
 // 2: local by hijerry17
 // 2: remote by hijerry17
+// 1: localBranch01 by hijerry17
 public class Main {
 
 	public static void main(String[] args) {
